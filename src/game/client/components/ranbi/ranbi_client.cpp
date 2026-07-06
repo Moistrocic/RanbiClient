85e4b0bc044f8558c88f628bf05884335420da00
Moistrocic <twrathe336@outlook.com>
2026-07-07 01:24:08 +0800

    添加功能：碰撞换肤

--- src/game/client/components/ranbi/ranbi_client.cpp
+++ src/game/client/components/ranbi/ranbi_client.cpp
@@ -1,8 +1,13 @@
 #include "ranbi_client.h"
 
 #include <base/system.h>
+
 #include <engine/shared/config.h>
 #include <engine/storage.h>
+
+#include <game/client/components/skins7.h>
+#include <game/client/gameclient.h>
+#include <game/gamecore.h>
 
 CRanbiClient::CRanbiClient()
 {
@@ -16,6 +21,96 @@ void CRanbiClient::OnConsoleInit()
 		pConfigManager->RegisterCallback(ConfigSaveCallback, this, ConfigDomain::RANBIWEAPONS);
 
 	Console()->Register("add_weapon_angle", "i[weapon_id] f[angle_start] f[angle_end] i[color] ?s[note]", CFGFLAG_CLIENT, ConAddWeaponAngle, this, "Add a weapon angle config");
+}
+
+void CRanbiClient::OnUpdate()
+{
+	// RANBICLIENT m_RcAutoChangeSkin
+	if(g_Config.m_RcAutoChangeSkin)
+	{
+		m_LastCollisionSkin.Reset();
+
+		for(int Dummy = 0; Dummy < NUM_DUMMIES; Dummy++)
+		{
+			int LocalId = GameClient()->m_aLocalIds[Dummy];
+			if(LocalId < 0 || LocalId >= MAX_CLIENTS)
+				continue;
+
+			const CGameClient::CClientData &LocalClient = GameClient()->m_aClients[LocalId];
+			if(!LocalClient.m_Active)
+				continue;
+
+			vec2 LocalPos = LocalClient.m_Predicted.m_Pos;
+			float ClosestDist = CCharacterCore::PhysicalSize() * 1.25f;
+			int ClosestId = -1;
+
+			for(int i = 0; i < MAX_CLIENTS; i++)
+			{
+				if(i == LocalId)
+					continue;
+
+				const CGameClient::CClientData &OtherClient = GameClient()->m_aClients[i];
+				if(!OtherClient.m_Active)
+					continue;
+
+				float Dist = distance(LocalPos, OtherClient.m_Predicted.m_Pos);
+				if(Dist < ClosestDist)
+				{
+					ClosestDist = Dist;
+					ClosestId = i;
+				}
+			}
+
+			if(ClosestId < 0)
+				continue;
+
+			const CGameClient::CClientData &CollidedClient = GameClient()->m_aClients[ClosestId];
+
+			m_LastCollisionSkin.m_ClientId = ClosestId;
+			m_LastCollisionSkin.m_Valid = true;
+
+			str_copy(m_LastCollisionSkin.m_aSkinName, CollidedClient.m_aSkinName);
+			m_LastCollisionSkin.m_UseCustomColor = CollidedClient.m_UseCustomColor;
+			m_LastCollisionSkin.m_ColorBody = CollidedClient.m_ColorBody;
+			m_LastCollisionSkin.m_ColorFeet = CollidedClient.m_ColorFeet;
+
+			// RANBICLIENT m_RcAutoChangeSkin: compare and replace skin if different
+			bool NeedChange = false;
+
+			if(str_comp(LocalClient.m_aSkinName, CollidedClient.m_aSkinName) != 0)
+				NeedChange = true;
+			else if(LocalClient.m_UseCustomColor != CollidedClient.m_UseCustomColor)
+				NeedChange = true;
+			else if(LocalClient.m_ColorBody != CollidedClient.m_ColorBody)
+				NeedChange = true;
+			else if(LocalClient.m_ColorFeet != CollidedClient.m_ColorFeet)
+				NeedChange = true;
+
+			if(NeedChange)
+			{
+				if(Dummy == 0)
+				{
+					str_copy(g_Config.m_ClPlayerSkin, CollidedClient.m_aSkinName);
+					g_Config.m_ClPlayerUseCustomColor = CollidedClient.m_UseCustomColor;
+					g_Config.m_ClPlayerColorBody = CollidedClient.m_ColorBody;
+					g_Config.m_ClPlayerColorFeet = CollidedClient.m_ColorFeet;
+				}
+				else
+				{
+					str_copy(g_Config.m_ClDummySkin, CollidedClient.m_aSkinName);
+					g_Config.m_ClDummyUseCustomColor = CollidedClient.m_UseCustomColor;
+					g_Config.m_ClDummyColorBody = CollidedClient.m_ColorBody;
+					g_Config.m_ClDummyColorFeet = CollidedClient.m_ColorFeet;
+				}
+
+				if(Dummy == 0)
+					GameClient()->SendInfo(false);
+				else
+					GameClient()->SendDummyInfo(false);
+			}
+			return;
+		}
+	}
 }
 
 void CRanbiClient::ConAddWeaponAngle(IConsole::IResult *pResult, void *pUserData)
--- src/game/client/components/ranbi/ranbi_client.h
+++ src/game/client/components/ranbi/ranbi_client.h
@@ -2,6 +2,9 @@
 #define GAME_CLIENT_COMPONENTS_RANBI_RANBI_CLIENT_H
 
 #include <engine/console.h>
+#include <engine/shared/protocol.h>
+
+#include <generated/protocol7.h>
 
 #include <game/client/component.h>
 
@@ -16,6 +19,28 @@ struct SWeaponInfo
 	char m_aNote[64];
 };
 
+struct SCollisionSkinInfo
+{
+	int m_ClientId;
+	bool m_Valid;
+
+	// 0.6/0.7 protocol skin info
+	char m_aSkinName[MAX_SKIN_LENGTH];
+	int m_UseCustomColor;
+	int m_ColorBody;
+	int m_ColorFeet;
+
+	void Reset()
+	{
+		m_ClientId = -1;
+		m_Valid = false;
+		m_aSkinName[0] = '\0';
+		m_UseCustomColor = 0;
+		m_ColorBody = 0;
+		m_ColorFeet = 0;
+	}
+};
+
 class CRanbiClient : public CComponent
 {
 public:
@@ -23,6 +48,9 @@ public:
 	int Sizeof() const override { return sizeof(*this); }
 
 	void OnConsoleInit() override;
+	void OnUpdate() override;
+
+	SCollisionSkinInfo m_LastCollisionSkin;
 
 	std::vector<SWeaponInfo> m_vWeapons;
 
