1a04ae0c1bc38e758f84557f8ad6d5ad7b113d58
Moistrocic <twrathe336@outlook.com>
2026-07-06 15:07:31 +0800

    添加Ranbi Binds文件

---
+++ src/engine/shared/config_variables_ranbi.h
@@ -0,0 +1,8 @@
+// This file can be included several times.
+
+#ifndef MACRO_CONFIG_INT
+#error "The config macros must be defined"
+#define MACRO_CONFIG_INT(Rcme, ScriptName, Def, Min, Max, Save, Desc) ;
+#define MACRO_CONFIG_COL(Rcme, ScriptName, Def, Save, Desc) ;
+#define MACRO_CONFIG_STR(Rcme, ScriptName, Len, Def, Save, Desc) ;
+#endif
